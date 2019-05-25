1e031f5c75e5163ed8f4230792f9be5f6344b342
softactor <tanveerqureshee1@gmail.com>
2019-05-25 14:00:14 +0600

    added country of origin

--- resources/views/backend/reports/plant_equipment_result_table.blade.php
+++ resources/views/backend/reports/plant_equipment_result_table.blade.php
@@ -6,6 +6,8 @@
                 <th>{{ trans('labels.backend.plantequipments.table.eel_code') }}</th>
                 <th>{{ trans('labels.backend.plantequipments.table.country_of_origin') }}</th>
                 <th>{{ trans('labels.backend.plantequipments.table.capacity') }}</th>
+                <th>Make By</th>
+                <th>Model</th>
                 <th>{{ trans('labels.backend.plantequipments.table.createdby') }}</th>
                 <th>{{ trans('labels.backend.plantequipments.table.createdat') }}</th>
             </tr>
@@ -18,9 +20,17 @@
                     <tr>
                         <td><?php echo $equipments->name; ?></td>
                         <td><?php echo $equipments->eel_code; ?></td>
-                        <td><?php echo $equipments->country_of_origin; ?></td>
+                        <td><?php echo getTableRawDetails('country', $equipments->country_of_origin)->nicename; ?></td>
                         <td><?php echo $equipments->capacity; ?></td>
-                        <td><?php echo $equipments->created_by; ?></td>
+                        <td><?php echo $equipments->make_by; ?></td>
+                        <td><?php echo $equipments->model; ?></td>
+                        <td>
+                            <?php
+                                $userDetails    =    get_user_details_by_user_id($equipments->created_by);
+                                echo $userDetails->first_name.' '.$userDetails->last_name;
+                            ?>
+                        </td>
+                        <td><?php echo date("F jS, Y", strtotime($equipments->created_at)); ?></td>
                         <td><?php echo $equipments->created_at; ?></td>
                     </tr>
                 <?php }
